32c61b18e635725759b05bcccdb16854ae38691c
xxxelppa <xxxelppa@naver.com>
2017-02-27 12:07:47 +0900

    Update ForTempTestRepoClass.java
    add Default Constructor

--- ForTempTestRepoClass.java
+++ ForTempTestRepoClass.java
@@ -1,5 +1,7 @@
 
 public class ForTempTestRepoClass {
+	// generate Default Constructor
+	ForTempTestRepoClass() { }
 	public static void main(String[] ar) {
 		System.out.println("Hello World!");
 	}
